61b442f7df9e37dfe3a1dabd916157a7475ca8d5
Saimun <123021229+saimun4all@users.noreply.github.com>
2026-05-25 12:23:50 -0400

    GENERATE A REPORT THAT SHOWS ALL KEY METRICS OF THE BUSINESS
    GENERATE A REPORT THAT SHOWS ALL KEY METRICS OF THE BUSINESS

--- scripts/measures_explore.sql
+++ scripts/measures_explore.sql
@@ -45,12 +45,46 @@ FROM `EDA.db`.gold_fact_sales
 WHERE order_number IS NOT NULL
 
 
+******* GENERATE REPORT THAT SHOWS ALL KEY METRICS OF THE BUSINESS *******
+
+SELECT
+	'Total Sales' AS measure_name,
+	SUM(sales_amount) AS measure_value
+FROM `EDA.db`.gold_fact_sales
+UNION ALL
+SELECT
+	'Total Quantity' AS measure_name,
+	SUM(quantity) AS measure_value
+FROM `EDA.db`.gold_fact_sales
+UNION ALL
+SELECT
+	'Average Price',	
+	AVG(price)
+FROM `EDA.db`.gold_fact_sales
+UNION ALL
+SELECT 
+	'Total Nr. Orders',
+	COUNT(DISTINCT order_number)
+FROM `EDA.db`.gold_fact_sales
+UNION ALL
+SELECT
+	'Total Nr. Products',
+	COUNT(DISTINCT product_key)
+FROM `EDA.db`.gold_fact_sales
+UNION ALL
+SELECT
+	'Total Nr. Customers',
+	COUNT(DISTINCT customer_key)
+FROM `EDA.db`.gold_fact_sales;
+
+
+
 -- Sum of Sales across categories
 
-SELECT  
-p.category,
-SUM(s.sales_amount)
-FROM `EDA.db`.gold_fact_sales s
-INNER JOIN `EDA.db`.gold_dim_products p
-ON s.product_key = p.product_key
-GROUP BY p.category 
+-- SELECT  
+-- p.category,
+-- SUM(s.sales_amount)
+-- FROM `EDA.db`.gold_fact_sales s
+-- INNER JOIN `EDA.db`.gold_dim_products p
+-- ON s.product_key = p.product_key
+-- GROUP BY p.category 
